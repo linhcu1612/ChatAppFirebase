040ccdab26286a76eccf02eca2966fddbd7da107
Nhat Linh <nhatlinh161232@gmail.com>
2021-08-15 08:35:29 +1000

    set up firebase and fb login config

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,17 @@
 /** @format */
-
+import firebase, { auth } from "../../firebase/config";
 import React from "react";
 import { Row, Col, Typography, Button } from "antd";
+
 const { Title } = Typography;
 
+const fbProvider = firebase.auth.FacebookAuthProvider();
+
 export default function Login() {
+  const handleFbLogin = () => {
+    auth.signInWithPopup(fbProvider);
+  };
+
   return (
     <div>
       <Row justify='center' style={{ height: 800 }}>
@@ -15,7 +22,9 @@ export default function Login() {
           <Button style={{ width: "100%", marginBottom: 5 }}>
             Login with Google
           </Button>
-          <Button style={{ width: "100%" }}>Login with Facebook</Button>
+          <Button style={{ width: "100%" }} onClick={handleFbLogin}>
+            Login with Facebook
+          </Button>
         </Col>
       </Row>
     </div>
