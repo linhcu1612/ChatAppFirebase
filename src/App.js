0b9e905f9065593128b233478f1db5cfb656549a
Nhat Linh <nhatlinh161232@gmail.com>
2021-08-15 09:19:55 +1000

    set up react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 /** @format */
 
 import Login from "./components/Login";
+import ChatRoom from "./components/ChatRoom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Login></Login>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Login} path='/login' />
+        <Route component={ChatRoom} path='/' />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ChatRoom/index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import React from "react";
+
+export default function ChatRoom() {
+  return <div>This is the chat room</div>;
+}
