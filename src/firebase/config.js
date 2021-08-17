27f9286a200b9cbe3cb70e1d4f6a46f3d8d5ac7b
Nhat Linh <nhatlinh161232@gmail.com>
2021-08-17 17:14:47 +1000

    hide config for firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyA9gzNft0SmshKPv3LdgrCKyaA9LNoZsH4",
-  authDomain: "chatapp-5c2b7.firebaseapp.com",
-  projectId: "chatapp-5c2b7",
-  storageBucket: "chatapp-5c2b7.appspot.com",
-  messagingSenderId: "774874534735",
-  appId: "1:774874534735:web:cd087b6111bd4c1ee6eb3a",
-  measurementId: "G-CQX3RX44LG",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
