d021c47cf89efc8755d37028e93fbadee5c1da70
Nhat Linh <nhatlinh161232@gmail.com>
2021-08-17 21:23:52 +1000

    reveal firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,13 +7,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
+  apiKey: "AIzaSyA9gzNft0SmshKPv3LdgrCKyaA9LNoZsH4",
+  authDomain: "1:774874534735:web:cd087b6111bd4c1ee6eb3a",
+  projectId: "chatapp-5c2b7.firebaseapp.com",
+  storageBucket: "chatapp-5c2b7.appspot.com",
+  messagingSenderId: "774874534735",
+  appId: "1:774874534735:web:cd087b6111bd4c1ee6eb3a",
+  measurementId: "G-CQX3RX44LG",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
